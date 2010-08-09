993fb308f43e86dabc3dd510e2a140c00665a20d
Scott Fritchie <scott@basho.com>
2010-08-09 16:22:57 -0500

    Add first of probably many QuickCheck tests

--- src/couch_merkle.erl
+++ src/couch_merkle.erl
@@ -52,7 +52,11 @@
 
 -include("couch_db.hrl").
 
--ifdef(EUNIT).
+-ifdef(TEST).
+-ifdef(EQC).
+-include_lib("eqc/include/eqc.hrl").
+-compile(export_all).
+-endif.
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
@@ -323,3 +327,169 @@ diff_merge([{KeyA,HashA}|ListA], [{KeyB,HashB}|ListB], Acc) when KeyA < KeyB ->
   diff_merge(ListA, [{KeyB,HashB}|ListB], [{KeyA,HashA}|Acc]);
 diff_merge([{KeyA,HashA}|ListA], [{KeyB,HashB}|ListB], Acc) when KeyA > KeyB ->
   diff_merge([{KeyA,HashA}|ListA], ListB, [{KeyB,HashB}|Acc]).
+
+-ifdef(EQC).
+
+%%% QuickCheck....
+
+%% 
+%% Example usage (your relative path may differ)
+%%
+%%  ../../rebar compile eunit
+%%  code:add_patha("./.eunit").
+%%  % Load the eunit version....
+%%  l(couch_merkle).
+%%  eqc:quickcheck(couch_merkle:prop_shuffle(80, 1)).
+%%
+%% MaxLen = Maximum length of the list of keys to put into a tree.
+%% MaxDeltas = Max length of the deltas to apply to MaxLen list:
+%%    insert, modify, and delete operations.
+%%
+%% N.B.: This test isn't actually testing a couch_btree delete operation!
+
+prop_shuffle(MaxLen, MaxDeltas) ->
+    ?FORALL(
+       {L1, Deltas},
+       {gen_list(MaxLen), gen_deltas(MaxDeltas)},
+       %% Instead of ?LET, use ?FORALL + noshrink() for reporting difference.
+       ?FORALL(
+          {L2, NumI, NumM, NumD},
+          noshrink(gen_2nd_list(L1, Deltas)),
+          begin
+              File1 = "./hack_diff_foo_1",
+              File2 = "./hack_diff_foo_2",
+              os:cmd("rm -rf " ++ File1 ++ " " ++ File2),
+              T1 = list_to_couch_merkle(File1, L1),
+              T2 = list_to_couch_merkle(File2, L2),
+              Diffs = couch_merkle:diff(T1, T2),
+              couch_merkle:close(T1),
+              couch_merkle:close(T2),
+              %% Fudge = 0,
+              InsertKeys = [K || {insert, K, _} <- Deltas],
+              DupInsertsP = not (lists:usort(InsertKeys) == lists:sort(InsertKeys)),
+              if
+                  DupInsertsP ->
+                      %% Call it true because this should be rare.
+                      true;
+                  length(Diffs) == (NumI + NumM + NumD) ->
+                      true;
+                 true ->
+                      {length(Diffs), NumI, NumM, NumD, Diffs}
+              end
+          end)).
+
+list_to_couch_merkle(File, L) ->
+    {ok, T1} = couch_merkle:open(File),
+    [T1 = couch_merkle:update(T1, K, H) || {K, H} <- L],
+    T1.
+
+%% Example usage: C1 = eqc:counterexample().
+%%                couch_merkle:counter_to_diff(C1).
+
+counter_to_diff(CounterExample) ->
+    [{L1, _}, {L2, _, _, _}] = CounterExample,
+    File1 = "hack_diff_foo_x",
+    File2 = "hack_diff_foo_y",
+    os:cmd("rm -rf " ++ File1 ++ " " ++ File2),
+    T1 = list_to_couch_merkle(File1, L1),
+    T2 = list_to_couch_merkle(File2, L2),
+    Diffs = couch_merkle:diff(T1, T2),
+    couch_merkle:close(T1),
+    couch_merkle:close(T2),
+    Diffs.    
+
+gen_list(MaxLen) ->
+    ?LET(Len, choose(0, MaxLen),
+         ?LET(L, vector(Len, noshrink({largeint(), largeint()})),
+              begin
+                  Ks = [abs(K) || {K, _} <- L],
+                  %% Recurse if there are duplicate keys
+                  case lists:usort(Ks) == lists:sort(Ks) of
+                      true  -> L;
+                      false -> gen_list(MaxLen)
+                  end
+              end)).
+
+gen_deltas(MaxDeltas) ->
+    oneof([
+           [],
+           ?LET(NumDeltas, choose(0, MaxDeltas),
+                vector(NumDeltas, gen_delta()))
+          ]).
+
+gen_delta() ->
+    %% This insert isn't 100% certain to avoid collisions with other
+    %% inserts, but we'll see how far it takes us before we have a
+    %% list of KVs that has a duplicate key.....
+    oneof([{insert, largeint(), largeint()},
+           {modify, largeint(), x},
+           {delete,          x, x}]).
+
+gen_largeint_prefix(L) ->
+    [{largeint(), X} || X <- L].
+
+%% For the sake of laziness, this func will also shuffle the list,
+%% which is probably what we want to do 100% of the time anyway.
+
+gen_2nd_list(L, Deltas) ->
+    ?LET({IL, IDeltas}, {gen_largeint_prefix(L), gen_largeint_prefix(Deltas)},
+         begin
+             ShuffleMix = lists:sort(IL ++ IDeltas),
+             apply_deltas([Thing || {_Rand, Thing} <- ShuffleMix])
+         end).
+
+apply_deltas(L) ->
+    apply_deltas(L, [], 0, 0, 0).
+
+apply_deltas([{insert, NewK, NewV}|Tail], Acc, NumI, NumM, NumD) ->
+    apply_deltas(Tail, [{NewK, NewV}|Acc], NumI + 1, NumM, NumD);
+apply_deltas([{modify, NewV, _}, {K, _OldV}|Tail], Acc, NumI, NumM, NumD) ->
+    apply_deltas(Tail, [{K, NewV}|Acc], NumI, NumM + 1, NumD);
+apply_deltas([{delete, _, _}, {_K, _OldV}|Tail], Acc, NumI, NumM, NumD) ->
+    apply_deltas(Tail, Acc, NumI, NumM, NumD + 1);
+apply_deltas([{_, _} = KV|Tail], Acc, NumI, NumM, NumD) ->
+    apply_deltas(Tail, [KV|Acc], NumI, NumM, NumD);
+apply_deltas([_Delta|Tail], Acc, NumI, NumM, NumD) ->
+    apply_deltas(Tail, Acc, NumI, NumM, NumD);
+apply_deltas([], Acc, NumI, NumM, NumD) ->
+    {Acc, NumI, NumM, NumD}.
+
+nthtail(Num, L) when Num > length(L) ->
+    [];
+nthtail(Num, L) ->
+    lists:nthtail(Num, L).
+
+%% SLF: Older testing stuff, before I decided that the complication of
+%% QuickCheck was going to be worthwhile after all.
+
+%% SLF: What a deal, diff seems to be [] (no differences) 100% of the
+%%      time for random lists of 150K elements.  And much much faster
+%%      than merkerl.erl when using update_many.  When using update on
+%%      each single key, (by eyeballing) I dunno which is faster.
+
+hack_diff(ListLen) ->
+    hack_diff(ListLen, 0).
+
+hack_diff(ListLen, NumToDel) ->
+    os:cmd("rm -rf ./hack_diff_1 ./hack_diff_2"),
+    L = [{X, random:uniform(200)} || X <- lists:seq(1, ListLen)],
+    _ExceptList = [random:uniform(ListLen) || _ <- lists:seq(1, NumToDel)],
+    {ok, T1} = couch_merkle:open("./hack_diff_1"),
+    L1 = hack_shuffle(L),
+    L2 = lists:nthtail(NumToDel, hack_shuffle(L)),
+%%    L2 = hack_shuffle([X || X <- L2a, not lists:member(X, ExceptList)]),
+    io:format("Shuffled lists equal: ~p, ", [L1 == L2]),
+    [T1 = couch_merkle:update(T1, K, H) || {K, H} <- L1],
+    %% T1 = couch_merkle:update_many(T1, L1),
+    io:format("T1 updated, "),
+    {ok, T2} = couch_merkle:open("./hack_diff_2"),
+    [T2 = couch_merkle:update(T2, K, H) || {K, H} <- L2],
+    %% T2 = couch_merkle:update_many(T2, L2),
+    io:format("T2 updated\n"),
+    couch_merkle:diff(T1, T2).
+
+hack_shuffle(L) ->
+    L2 = [{random:uniform(100), X} || X <- L],
+    [X || {_, X} <- lists:sort(L2)].
+
+-endif.   %% EQC
