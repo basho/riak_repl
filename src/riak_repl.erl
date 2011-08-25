e0526afdf34f128c2d31b7c50298a3af2d51fea3
Andrew Thompson <andrew@hijacked.us>
2011-08-25 18:32:02 -0400

    Change repl to use a fixup_bucket function
    az618

--- src/riak_repl.erl
+++ src/riak_repl.erl
@@ -5,6 +5,7 @@
 -include("riak_repl.hrl").
 -export([start/0, stop/0]).
 -export([install_hook/0]).
+-export([fixup/2]).
 
 start() ->
     riak_core_util:start_app_deps(riak_repl),
@@ -18,7 +19,38 @@ install_hook() ->
     {ok, DefaultBucketProps} = application:get_env(riak_core, 
                                                    default_bucket_props),
     application:set_env(riak_core, default_bucket_props, 
-                        proplists:delete(postcommit, DefaultBucketProps)),
-    riak_core_bucket:append_bucket_defaults([{postcommit, [?REPL_HOOK]}]),
+                        proplists:delete(repl, DefaultBucketProps)),
+    riak_core_bucket:append_bucket_defaults([{repl, true}]),
     ok.
+
+fixup(_Bucket, BucketProps) ->
+    case proplists:get_value(repl, BucketProps) of
+        true ->
+            CleanPostcommit = strip_postcommit(BucketProps),
+            UpdPostcommit = CleanPostcommit ++ [?REPL_HOOK],
+
+            {ok, lists:keyreplace(postcommit, 1, BucketProps, 
+                    {postcommit, UpdPostcommit})};
+        _ ->
+           %% remove the postcommit hook, if any
+            CleanPostcommit = strip_postcommit(BucketProps),
+            %% Update the bucket properties
+            UpdBucketProps = lists:keyreplace(postcommit, 1, BucketProps, 
+                {postcommit, CleanPostcommit}),
+            {ok, UpdBucketProps}
+    end.
+
+%% Get the postcommit hook from the bucket and strip any
+%% existing repl hooks.
+strip_postcommit(BucketProps) ->
+    %% Get the current postcommit hook
+    case proplists:get_value(postcommit, BucketProps, []) of
+        X when is_list(X) ->
+            CurrentPostcommit=X;
+        {struct, _}=X ->
+            CurrentPostcommit=[X]
+    end,
     
+    %% Add kv/search hook - make sure there are not duplicate entries
+    CurrentPostcommit -- [?REPL_HOOK].
+
--- src/riak_repl_app.erl
+++ src/riak_repl_app.erl
@@ -15,6 +15,8 @@ start(_Type, _StartArgs) ->
     application:set_env(riak_repl, incarnation, IncarnationId),
     ok = ensure_dirs(),
 
+    riak_core:register_application([{bucket_fixup, riak_repl}]),
+
     %% Register our cluster_info app callback modules, with catch if
     %% the app is missing or packaging is broken.
     catch cluster_info:register_app(riak_repl_cinfo),
