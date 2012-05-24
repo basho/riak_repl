1bd92e5dcab67c77702c819fa18cc6e5460fd5cc
Russell Brown <russell.brown@mac.com>
2012-05-24 13:34:05 +0100

    Namespace the stats with the app name in a 2 tuple
    Makes querying easier. Less verbose for developer.

--- src/riak_repl_stats.erl
+++ src/riak_repl_stats.erl
@@ -30,61 +30,61 @@
          register_stats/0,
          get_stats/0]).
 
--define(APP_NAME, "riak_repl_").
+-define(APP, riak_repl).
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 register_stats() ->
     [register_stat(Name, Type) || {Name, Type} <- stats()],
-    folsom_metrics:notify_existing_metric(riak_repl_last_report, now(), gauge).
+    folsom_metrics:notify_existing_metric({?APP, last_report}, now(), gauge).
 
 client_bytes_sent(Bytes) ->
-    increment_counter(riak_repl_client_bytes_sent, Bytes).
+    increment_counter(client_bytes_sent, Bytes).
 
 client_bytes_recv(Bytes) ->
-    increment_counter(riak_repl_client_bytes_recv, Bytes).
+    increment_counter(client_bytes_recv, Bytes).
 
 client_connects() ->
-    increment_counter(riak_repl_client_connects).
+    increment_counter(client_connects).
     
 client_connect_errors() ->
-    increment_counter(riak_repl_client_connect_errors).
+    increment_counter(client_connect_errors).
 
 client_redirect() ->
-    increment_counter(riak_repl_client_redirect).
+    increment_counter(client_redirect).
 
 server_bytes_sent(Bytes) ->
-    increment_counter(riak_repl_server_bytes_sent, Bytes).
+    increment_counter(server_bytes_sent, Bytes).
 
 server_bytes_recv(Bytes) ->
-    increment_counter(riak_repl_server_bytes_recv, Bytes).
+    increment_counter(server_bytes_recv, Bytes).
 
 server_connects() ->
-    increment_counter(riak_repl_server_connects).
+    increment_counter(server_connects).
 
 server_connect_errors() ->
-    increment_counter(riak_repl_server_connect_errors).
+    increment_counter(server_connect_errors).
 
 server_fullsyncs() ->
-    increment_counter(riak_repl_server_fullsyncs).
+    increment_counter(server_fullsyncs).
 
 objects_dropped_no_clients() ->
-    increment_counter(riak_repl_objects_dropped_no_clients).
+    increment_counter(objects_dropped_no_clients).
 
 objects_dropped_no_leader() ->
-    increment_counter(riak_repl_objects_dropped_no_leader).
+    increment_counter(objects_dropped_no_leader).
 
 objects_sent() ->
-    increment_counter(riak_repl_objects_sent).
+    increment_counter(objects_sent).
 
 objects_forwarded() ->
-    increment_counter(riak_repl_objects_forwarded).
+    increment_counter(objects_forwarded).
 
 elections_elected() ->
-    increment_counter(riak_repl_elections_elected).
+    increment_counter(elections_elected).
 
 elections_leader_changed() ->
-    increment_counter(riak_repl_elections_leader_changed).
+    increment_counter(elections_leader_changed).
 
 get_stats() ->
     lists:flatten([backwards_compat(Stat, Type) ||
@@ -95,45 +95,45 @@ init([]) ->
     {ok, ok}.
 
 register_stat(Name, counter) ->
-    folsom_metrics:new_counter(Name);
+    folsom_metrics:new_counter({?APP, Name});
 register_stat(Name, history) ->
     BwHistoryLen =  get_bw_history_len(),
-    folsom_metrics:new_history(Name, BwHistoryLen);
+    folsom_metrics:new_history({?APP, Name}, BwHistoryLen);
 register_stat(Name, gauge) ->
-    folsom_metrics:new_gauge(Name).
+    folsom_metrics:new_gauge({?APP, Name}).
 
 stats() ->
-    [{riak_repl_server_bytes_sent, counter},
-     {riak_repl_server_bytes_recv, counter},
-     {riak_repl_server_connects, counter},
-     {riak_repl_server_connect_errors, counter},
-     {riak_repl_server_fullsyncs, counter},
-     {riak_repl_client_bytes_sent, counter},
-     {riak_repl_client_bytes_recv, counter},
-     {riak_repl_client_connects, counter},
-     {riak_repl_client_connect_errors, counter},
-     {riak_repl_client_redirect, counter},
-     {riak_repl_objects_dropped_no_clients, counter},
-     {riak_repl_objects_dropped_no_leader, counter},
-     {riak_repl_objects_sent, counter},
-     {riak_repl_objects_forwarded, counter},
-     {riak_repl_elections_elected, counter},
-     {riak_repl_elections_leader_change, counter},
-     {riak_repl_client_rx_kbps, history},
-     {riak_repl_client_tx_kbps, history},
-     {riak_repl_server_rx_kbps, history},
-     {riak_repl_server_tx_kbps, history},
-     {riak_repl_last_report, gauge},
-     {riak_repl_last_client_bytes_sent, gauge},
-     {riak_repl_last_client_bytes_recv, gauge},
-     {riak_repl_last_server_bytes_sent, gauge},
-     {riak_repl_last_server_bytes_recv, gauge}].
+    [{server_bytes_sent, counter},
+     {server_bytes_recv, counter},
+     {server_connects, counter},
+     {server_connect_errors, counter},
+     {server_fullsyncs, counter},
+     {client_bytes_sent, counter},
+     {client_bytes_recv, counter},
+     {client_connects, counter},
+     {client_connect_errors, counter},
+     {client_redirect, counter},
+     {objects_dropped_no_clients, counter},
+     {objects_dropped_no_leader, counter},
+     {objects_sent, counter},
+     {objects_forwarded, counter},
+     {elections_elected, counter},
+     {elections_leader_change, counter},
+     {client_rx_kbps, history},
+     {client_tx_kbps, history},
+     {server_rx_kbps, history},
+     {server_tx_kbps, history},
+     {last_report, gauge},
+     {last_client_bytes_sent, gauge},
+     {last_client_bytes_recv, gauge},
+     {last_server_bytes_sent, gauge},
+     {last_server_bytes_recv, gauge}].
 
 increment_counter(Name) ->
     increment_counter(Name, 1).
 
 increment_counter(Name, IncrBy) when is_atom(Name) andalso is_integer(IncrBy) ->
-    folsom_metrics:notify_existing_metric(Name, {inc, IncrBy}, counter).
+    folsom_metrics:notify_existing_metric({?APP, Name}, {inc, IncrBy}, counter).
 
 handle_call(_Req, _From, State) ->
     {reply, ok, State}.
@@ -142,29 +142,29 @@ handle_cast(_Msg, State) ->
     {noreply, State}.
 
 handle_info(report_bw, State) ->
-    ThisClientBytesSent=lookup_stat(riak_repl_client_bytes_sent),
-    ThisClientBytesRecv=lookup_stat(riak_repl_client_bytes_recv),
-    ThisServerBytesSent=lookup_stat(riak_repl_server_bytes_sent),
-    ThisServerBytesRecv=lookup_stat(riak_repl_server_bytes_recv),
+    ThisClientBytesSent=lookup_stat(client_bytes_sent),
+    ThisClientBytesRecv=lookup_stat(client_bytes_recv),
+    ThisServerBytesSent=lookup_stat(server_bytes_sent),
+    ThisServerBytesRecv=lookup_stat(server_bytes_recv),
 
     Now = now(),
-    DeltaSecs = now_diff(Now, lookup_stat(riak_repl_last_report)),
-    ClientTx = bytes_to_kbits_per_sec(ThisClientBytesSent, lookup_stat(riak_repl_last_client_bytes_sent), DeltaSecs),
-    ClientRx = bytes_to_kbits_per_sec(ThisClientBytesRecv, lookup_stat(riak_repl_last_client_bytes_recv), DeltaSecs),
-    ServerTx = bytes_to_kbits_per_sec(ThisServerBytesSent, lookup_stat(riak_repl_last_server_bytes_sent), DeltaSecs),
-    ServerRx = bytes_to_kbits_per_sec(ThisServerBytesRecv, lookup_stat(riak_repl_last_server_bytes_recv), DeltaSecs),
+    DeltaSecs = now_diff(Now, lookup_stat(last_report)),
+    ClientTx = bytes_to_kbits_per_sec(ThisClientBytesSent, lookup_stat(last_client_bytes_sent), DeltaSecs),
+    ClientRx = bytes_to_kbits_per_sec(ThisClientBytesRecv, lookup_stat(last_client_bytes_recv), DeltaSecs),
+    ServerTx = bytes_to_kbits_per_sec(ThisServerBytesSent, lookup_stat(last_server_bytes_sent), DeltaSecs),
+    ServerRx = bytes_to_kbits_per_sec(ThisServerBytesRecv, lookup_stat(last_server_bytes_recv), DeltaSecs),
 
-    [folsom_metrics:notify_existing_metric(Metric, Reading, history)
-     || {Metric, Reading} <- [{riak_repl_client_tx_kbps, ClientTx},
-                              {riak_repl_client_rx_kbps, ClientRx},
-                              {riak_repl_server_tx_kbps, ServerTx},
-                              {riak_repl_server_rx_kbps, ServerRx}]],
+    [folsom_metrics:notify_existing_metric({?APP, Metric}, Reading, history)
+     || {Metric, Reading} <- [{client_tx_kbps, ClientTx},
+                              {client_rx_kbps, ClientRx},
+                              {server_tx_kbps, ServerTx},
+                              {server_rx_kbps, ServerRx}]],
 
-    [folsom_metrics:notify_existing_metric(Metric, Reading, gauge)
-     || {Metric, Reading} <- [{riak_repl_last_client_bytes_sent, ThisClientBytesSent},
-                              {riak_repl_last_client_bytes_recv, ThisClientBytesRecv},
-                              {riak_repl_last_server_bytes_sent, ThisServerBytesSent},
-                              {riak_repl_last_server_bytes_recv, ThisServerBytesRecv}]],
+    [folsom_metrics:notify_existing_metric({?APP, Metric}, Reading, gauge)
+     || {Metric, Reading} <- [{last_client_bytes_sent, ThisClientBytesSent},
+                              {last_client_bytes_recv, ThisClientBytesRecv},
+                              {last_server_bytes_sent, ThisServerBytesSent},
+                              {last_server_bytes_recv, ThisServerBytesRecv}]],
     
     schedule_report_bw(),
     {noreply, State};
@@ -182,7 +182,7 @@ bytes_to_kbits_per_sec(This, Last, Delta) ->
     trunc((This - Last) / (128 * Delta)).  %% x8/1024 = x/128
 
 lookup_stat(Name) ->
-    folsom_metrics:get_metric_value(Name).
+    folsom_metrics:get_metric_value({?APP, Name}).
 
 now_diff({Lmega,Lsecs,_Lmicro}, {Emega,Esecs,_Emicro}) ->
     1000000*(Lmega-Emega)+(Lsecs-Esecs).
@@ -191,13 +191,10 @@ get_bw_history_len() ->
     app_helper:get_env(riak_repl, bw_history_len, 8).
 
 backwards_compat(Name, history) ->
-    Stats = folsom_metrics:get_history_values(Name, get_bw_history_len()),
+    Stats = folsom_metrics:get_history_values({?APP, Name}, get_bw_history_len()),
     Readings = [[Reading || {event, Reading} <- Events] || {_Moment, Events} <- Stats],
-    {un_namespace(Name), Readings};
+    {Name, Readings};
 backwards_compat(_Name, gauge) ->
     [];
 backwards_compat(Name,  _Type) ->
-    {un_namespace(Name), lookup_stat(Name)}.
-
-un_namespace(Name) ->
-    list_to_atom(lists:subtract(atom_to_list(Name), ?APP_NAME)).
+    {Name, lookup_stat(Name)}.
