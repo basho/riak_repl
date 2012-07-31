acd95280bdc7e41c45cfc851b3b11bec12b04249
Chris Tilt <chris.tilt@gmail.com>
2012-07-31 07:14:07 -0700

    Change full-sync strategy to keep tcp pipe full of sync data to client

--- src/riak_repl_fullsync_helper.erl
+++ src/riak_repl_fullsync_helper.erl
@@ -393,7 +393,7 @@ diff_keys(R, L, #diff_state{replies=0, fsm=FSM, ref=Ref, count=Count} = DiffStat
             lager:info("stop request while diffing"),
             DiffState;
         {Ref, diff_resume} ->
-            %lager:info("resuming diff stream"),
+            lager:info("resuming diff stream"),
             diff_keys(R, L, DiffState#diff_state{replies=Count})
     end;
 diff_keys({{Key, Hash}, RNext}, {{Key, Hash}, LNext}, DiffState) ->
