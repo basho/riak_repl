563391df07999be83c7000bc0763b8ae02146053
Andy Gross <andy@andygross.org>
2012-08-01 13:40:15 -0700

    expose replication stats over HTTP.

---
+++ src/riak_repl_wm_stats.erl
@@ -0,0 +1,82 @@
+%% -------------------------------------------------------------------
+%%
+%% riak_repl_wm_stats: publishing Riak replication stats via HTTP
+%%
+%% Copyright (c) 2007-2012 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(riak_repl_wm_stats).
+
+-export([
+         init/1,
+         encodings_provided/2,
+         content_types_provided/2,
+         service_available/2,
+         forbidden/2,
+         produce_body/2,
+         pretty_print/2
+        ]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+-record(ctx, {}).
+
+init(_) ->
+    {ok, #ctx{}}.
+
+%% @spec encodings_provided(webmachine:wrq(), context()) ->
+%%         {[encoding()], webmachine:wrq(), context()}
+%% @doc Get the list of encodings this resource provides.
+%%      "identity" is provided for all methods, and "gzip" is
+%%      provided for GET as well
+encodings_provided(ReqData, Context) ->
+    case wrq:method(ReqData) of
+        'GET' ->
+            {[{"identity", fun(X) -> X end},
+              {"gzip", fun(X) -> zlib:gzip(X) end}], ReqData, Context};
+        _ ->
+            {[{"identity", fun(X) -> X end}], ReqData, Context}
+    end.
+
+%% @spec content_types_provided(webmachine:wrq(), context()) ->
+%%          {[ctype()], webmachine:wrq(), context()}
+%% @doc Get the list of content types this resource provides.
+%%      "application/json" and "text/plain" are both provided
+%%      for all requests.  "text/plain" is a "pretty-printed"
+%%      version of the "application/json" content.
+content_types_provided(ReqData, Context) ->
+    {[{"application/json", produce_body},
+      {"text/plain", pretty_print}],
+     ReqData, Context}.
+
+service_available(ReqData, Ctx) ->
+    {true, ReqData, Ctx}.
+
+forbidden(RD, Ctx) ->
+    {riak_kv_wm_utils:is_forbidden(RD), RD, Ctx}.
+
+produce_body(ReqData, Ctx) ->
+    Body = mochijson2:encode({struct, riak_repl_stats:get_stats()}),
+    {Body, ReqData, Ctx}.
+
+%% @spec pretty_print(webmachine:wrq(), context()) ->
+%%          {string(), webmachine:wrq(), context()}
+%% @doc Format the respons JSON object is a "pretty-printed" style.
+pretty_print(RD1, C1=#ctx{}) ->
+    {Json, RD2, C2} = produce_body(RD1, C1),
+    {json_pp:print(binary_to_list(list_to_binary(Json))), RD2, C2}.
