b21928463d9c643f8e63a5770e558e606bc7387e
Chris Tilt <chris.tilt@gmail.com>
2012-08-21 11:50:45 -0400

    fix protocol error with number of syncs sent to client and remove lager:info

--- src/riak_repl_keylist_server.erl
+++ src/riak_repl_keylist_server.erl
@@ -135,6 +135,7 @@ wait_for_partition({partition, Partition}, State = #state{work_dir=WorkDir}) ->
     {next_state, build_keylist, State#state{kl_pid=KeyListPid,
             kl_ref=KeyListRef, kl_fn=KeyListFn,
             partition=Partition, partition_start=now(), stage_start=now(),
+            pending_acks=0, generator_paused=false,
             their_kl_fn=TheirKeyListFn, their_kl_fh=undefined}};
 wait_for_partition(Event, State) ->
     lager:debug("Full-sync with site ~p; ignoring event ~p",
@@ -226,10 +227,10 @@ wait_keylist(kl_eof, #state{their_kl_fh=FH} = State) ->
     %% to add some backpressure, while keeping the pipeline full (as long as the
     %% client continues to reply diff_ack).
     lager:info("Full-sync for ~p using ~p windows of batch_size ~p",
-               [State#state.partition, ?ACKS_IN_FLIGHT, State#state.diff_batch_size / ?ACKS_IN_FLIGHT]),
+               [State#state.partition, ?ACKS_IN_FLIGHT, State#state.diff_batch_size div ?ACKS_IN_FLIGHT]),
     {ok, Ref} = riak_repl_fullsync_helper:diff_stream(Pid, State#state.partition,
         State#state.kl_fn, State#state.their_kl_fn,
-        State#state.diff_batch_size / ?ACKS_IN_FLIGHT),
+        State#state.diff_batch_size div ?ACKS_IN_FLIGHT),
     {next_state, diff_keylist, State#state{diff_ref=Ref, diff_pid=Pid,
             stage_start=now()}};
 wait_keylist({skip_partition, Partition}, #state{partition=Partition} = State) ->
@@ -268,12 +269,10 @@ diff_keylist({Ref, diff_paused}, #state{socket=Socket, transport=Transport,
     WorkerPaused = case PendingAcks < ?ACKS_IN_FLIGHT of
                        true ->
                            %% another batch can be sent immediately
-                           lager:info("resuming diff generator with ~p pending acks",[PendingAcks]),
                            State#state.diff_pid ! {Ref, diff_resume},
                            false;
                        false ->
                            %% already ACKS_IN_FLIGHT batches out. Don't resume yet.
-                           lager:info("diff generator paused with ~p pending acks",[PendingAcks]),
                            true
                    end,
     {next_state, diff_keylist, State#state{pending_acks=PendingAcks, generator_paused=WorkerPaused}};
@@ -283,7 +282,6 @@ diff_keylist({diff_ack, Partition}, #state{partition=Partition, diff_ref=Ref,
                                            pending_acks=PendingAcks0} = State) ->
     %% That's one less "pending" ack from the client. Tell client to keep going.
     PendingAcks = PendingAcks0-1,
-    riak_repl_tcp_server:send(Transport, Socket, {diff_ack, Partition}),
     %% If the generator was paused, resume it. That would happen if there are already
     %% ACKS_IN_FLIGHT batches in flight. Better to check "paused" state than guess by
     %% pending acks count.
