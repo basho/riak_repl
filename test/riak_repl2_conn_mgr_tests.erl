002b566620f3c45595a21a7d384bb0f4bd9d2957
Chris Tilt <chris.tilt@gmail.com>
2012-08-27 21:59:03 -0700

    Added better failure detection. Notification of client of failed negotiation. Improved test verification. Removed debug statemetnst. Ready to play with.

--- test/riak_repl2_conn_mgr_tests.erl
+++ test/riak_repl2_conn_mgr_tests.erl
@@ -2,21 +2,14 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([test1service/4, test2service/4, connected/5, connect_failed/3]).
+-export([test1service/4, connected/5, connect_failed/3]).
 
 %% host service functions
-test1service(_Socket, _Transport, {Proto, MyVer, RemoteVer}, Args) ->
+test1service(_Socket, _Transport, {error, Reason}, Args) ->
+    ?assert(Args == failed_host_args),
+    ?assert(Reason == protocol_version_not_supported);
+test1service(_Socket, _Transport, {ok, {Proto, MyVer, RemoteVer}}, Args) ->
     [ExpectedMyVer, ExpectedRemoteVer] = Args,
-    ?debugFmt("started test1service mine = ~p, remote = ~p", [MyVer, RemoteVer]),
-    ?assert(ExpectedMyVer == MyVer),
-    ?assert(ExpectedRemoteVer == RemoteVer),
-    ?assert(Proto == test1proto),
-    timer:sleep(2000),
-    {ok, self()}.
-
-test2service(_Socket, _Transport, {Proto, MyVer, RemoteVer}, Args) ->
-    [ExpectedMyVer, ExpectedRemoteVer] = Args,
-%%    ?debugFmt("started test2service mine = ~p, remote = ~p", [MyVer, RemoteVer]),
     ?assert(ExpectedMyVer == MyVer),
     ?assert(ExpectedRemoteVer == RemoteVer),
     ?assert(Proto == test1proto),
@@ -26,20 +19,18 @@ test2service(_Socket, _Transport, {Proto, MyVer, RemoteVer}, Args) ->
 %% client connection callbacks
 connected(_Socket, _Transport, {_IP, _Port}, {Proto, MyVer, RemoteVer}, Args) ->
     [ExpectedMyVer, ExpectedRemoteVer] = Args,
-%%    ?debugFmt("client connected on {~p,~p} with args ~p, mine = ~p remote = ~p",
-%%              [_IP, _Port, Args, MyVer, RemoteVer]),
     ?assert(Proto == test1proto),
     ?assert(ExpectedMyVer == MyVer),
     ?assert(ExpectedRemoteVer == RemoteVer),
     timer:sleep(2000).
 
 connect_failed({Proto,_Vers}, {error, Reason}, Args) ->
-    ?debugFmt("connect_failed: Proto = ~p, error = ~p", [Proto, Reason]),
-    ?assert(Args == some_args),
+    ?assert(Args == failed_client_args),
+    ?assert(Reason == protocol_version_not_supported),
     ?assert(Proto == test1protoFailed).
 
 
-handshake_test() ->
+protocol_match_test() ->
     %% start ranch as an application so that we have a supervision tree,
     %% otherwise ranch will crash with a noproc in gen_server call.
     application:start(ranch),
@@ -54,9 +45,7 @@ handshake_test() ->
                   {active, false}],
     %% start dispatcher
     MaxListeners = 10,
-    SubProtocols = [{{test1proto, [{2,1}, {1,0}]}, ?MODULE, test1service, [{1,0}, {1,1}]},
-                    {{test2proto, [{3,4}]}, ?MODULE, test2service, []}
-                   ],
+    SubProtocols = [{{test1proto, [{2,1}, {1,0}]}, ?MODULE, test1service, [{1,0}, {1,1}]}],
     riak_repl2_conn_mgr:start_dispatcher({IP,Port}, MaxListeners, TcpOptions, SubProtocols),
 
     %% try to connect via a client that speaks 0.1 and 1.1
@@ -67,6 +56,8 @@ handshake_test() ->
     application:stop(ranch),
     ok.
 
+%% test that a mismatch of client and host args will notify both host and client
+%% of a failed negotiation.
 failed_protocol_match_test() ->
     %% start ranch as an application so that we have a supervision tree,
     %% otherwise ranch will crash with a noproc in gen_server call.
@@ -82,13 +73,12 @@ failed_protocol_match_test() ->
                   {active, false}],
     %% start dispatcher
     MaxListeners = 10,
-    SubProtocols = [{{test1protoFailed, [{2,1}, {1,0}]}, ?MODULE, test1service, [{1,0}, {1,1}]}
-                   ],
+    SubProtocols = [{{test1protoFailed, [{2,1}, {1,0}]}, ?MODULE, test1service, [failed_host_args]}],
     riak_repl2_conn_mgr:start_dispatcher({IP,Port}, MaxListeners, TcpOptions, SubProtocols),
 
     %% try to connect via a client that speaks 0.1 and 3.1. No Match with host!
     ClientProtocol = {test1protoFailed, [{0,1},{3,1}]},
-    riak_repl2_conn_mgr:connect({IP,Port}, ClientProtocol, TcpOptions, {?MODULE, some_args}),
+    riak_repl2_conn_mgr:connect({IP,Port}, ClientProtocol, TcpOptions, {?MODULE, failed_client_args}),
 
     timer:sleep(2000),
     application:stop(ranch),
