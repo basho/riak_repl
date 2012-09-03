d14f509048af38bea88d2bd3b44c2624b084cafa
Jon Meredith <jmeredith@basho.com>
2012-09-03 15:56:59 -0600

    Added missing supervisors

---
+++ src/riak_repl2_rtsink_conn_sup.erl
@@ -0,0 +1,33 @@
+%% Riak EnterpriseDS
+%% Copyright 2007-2012 Basho Technologies, Inc. All Rights Reserved.
+-module(riak_repl2_rtsink_conn_sup).
+-behaviour(supervisor).
+-export([start_link/0, enable/1, disable/1, enabled/0]).
+-export([init/1]).
+
+-define(SHUTDOWN, 5000). % how long to give rtsource processes to persist queue/shutdown
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%%TODO: Rename enable/disable something better - start/stop is a bit overloaded
+enable(Remote) ->
+    lager:info("Starting replication realtime source ~p", [Remote]),
+    ChildSpec = make_remote(Remote),
+    supervisor:start_child(?MODULE, ChildSpec).
+
+disable(Remote) ->
+    lager:info("Stopping replication realtime source ~p", [Remote]),
+    supervisor:terminate_child(?MODULE, Remote),
+    supervisor:delete_child(?MODULE, Remote).
+
+enabled() ->
+    [{Remote, Pid} || {Remote, Pid, _, _} <- supervisor:which_children(?MODULE)].
+
+%% @private
+init([]) ->
+    {ok, {{one_for_one, 10, 10}, []}}.
+
+make_remote(Remote) ->
+    {Remote, {riak_repl2_rtsink_conn, start_link, [Remote]},
+        permanent, ?SHUTDOWN, worker, [riak_repl2_rtsink_conn]}.
