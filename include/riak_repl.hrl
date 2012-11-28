800b22db7208e33cad23fcd19edfd0d2d906acfa
Dave Parfitt <dparfitt@basho.com>
2012-11-28 11:13:22 -0500

    fs max_fssource_node & max_fssource_cluster cmds
    allow max_fssource_node and max_fssource_cluster to be changed at
    runtime from the riak-repl shell script.
    
    Note: A fullsync stop and start is required for these values to take
    effect.
    
    see ticket https://help.basho.com/tickets/3024 for more info

--- include/riak_repl.hrl
+++ include/riak_repl.hrl
@@ -20,6 +20,8 @@
 -define(TCP_MON_RT_APP, repl_rt).
 -define(TCP_MON_FULLSYNC_APP, repl_fullsync).
 -define(DEFAULT_REPL_MODE, mode_repl12).
+-define(DEFAULT_SOURCE_PER_NODE, 2).
+-define(DEFAULT_SOURCE_PER_CLUSTER, 5).
 
 -type(ip_addr_str() :: string()).
 -type(ip_portnum() :: non_neg_integer()).
