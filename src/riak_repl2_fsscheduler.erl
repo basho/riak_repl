aa2d5fd065975b962cc90b1cafc7e1c0e2cfe919
Chris Tilt <chris.tilt@gmail.com>
2012-12-04 18:39:58 -0800

    Added everything but the validity check...and testing of course.

--- src/riak_repl2_fsscheduler.erl
+++ src/riak_repl2_fsscheduler.erl
@@ -26,6 +26,14 @@
 -define(MIN_TEMP, 1.0).
 -define(MAX_ITERATIONS, 50).
 
+-record(constraints, {
+          max_sources,
+          max_sinks,
+          max_total,
+          sources,
+          sinks
+    }).
+
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
@@ -40,30 +48,158 @@
 %% No sink node will  appear in the combined work more than SinkMax times.
 %%
 schedule(CurrentWork, Partitions, SourceOwners, SinkOwners, SourceMax, SinkMax, TotalMax) ->
-    anneal(CurrentWork, [], Partitions, SourceOwners, SinkOwners, SourceMax, SinkMax, TotalMax, ?MAX_TEMP).
-
+    Constraints = #constraints{max_sources=SourceMax, max_sinks=SinkMax, max_total=TotalMax,
+                              sources=SourceOwners, sinks=SinkOwners },
+    anneal(CurrentWork, [], Partitions, Constraints, ?MAX_TEMP).
 
 %% Anneal until we reach the lower temperature bound
-anneal(_Cur, Solution, _Partitions, _Sources, _Sinks, _SourceMax, _SinkMax, _TotalMax, Temp) when Temp < ?MIN_TEMP ->
+anneal(_Cur, Solution, _Partitions, _Constraints, Temp) when Temp < ?MIN_TEMP ->
+    %% cool enough. we're done.
     Solution;
-anneal(Cur, Solution, Partitions, Sources, Sinks, SourceMax, SinkMax, TotalMax, Temp) ->
-    %% try 
-    annealTemp(Cur, Solution, Partitions, Sources, Sinks, SourceMax, SinkMax, TotalMax, Temp, ?MAX_ITERATIONS),
+anneal(Cur, Solution, Partitions, Constraints, Temp) ->
+    %% anneal at temperature for some iterations
+    annealTemp(Cur, Solution, Partitions, Constraints, Temp, ?MAX_ITERATIONS),
     %% reduce temperature and try again
     T = 0.90 * Temp,
-    anneal(Cur, Solution, Partitions, Sources, Sinks, SourceMax, SinkMax, TotalMax, T).
+    anneal(Cur, Solution, Partitions, Constraints, T).
 
 %% Run Iterations number of iterations at this temperature
-annealTemp(_Cur, Solution, _Partitions, _Sources, _Sinks, _SourceMax, _SinkMax, _TotalMax, _Temp, 0) ->
+annealTemp(_Cur, Solution, _Partitions, _Constraints, _Temp, 0) ->
     Solution;
-annealTemp(Cur, Solution, Partitions, Sources, Sinks, SourceMax, SinkMax, TotalMax, Temp, Iterations) ->
-    annealTemp(Cur, Solution, Partitions, Sources, Sinks, SourceMax, SinkMax, TotalMax, Temp, Iterations-1).
+annealTemp(Cur, {OldSolution, OldScore}=Best, Partitions, Contraints, Temp, Iterations) ->
+    {NewSolution, NewScore} = randomize_schedule(Cur, OldSolution, Partitions, Contraints, Temp),
+    NewBest = case NewScore > OldScore of
+                  true -> %% improved
+                      {NewSolution, NewScore};
+                  false -> %% no improvement
+                      Best
+              end,
+    annealTemp(Cur, NewBest, Partitions, Contraints, Temp, Iterations-1).
+
+%% Return a new valid schedule.
+%% higher temperatures are more likely to add new partitions
+%% lower temperatures will share between removes and swaps
+randomize_schedule(Cur, Solution, Partitions, Constraints, Temp) ->
+    NotEmpty = Solution =/= [],
+    NotMaxed = (length(Cur) + length(Solution)) < Constraints#constraints.max_total,
+    New = case (Temp/?MAX_TEMP) > random:uniform() and NotMaxed of
+              true -> %% try adding a new partition
+                  Candidates = filter(Cur, Solution, Partitions),
+                  add_partition(Solution, Candidates);
+              false -> %% try remove or swap
+                  case random:uniform() > 0.5 and NotEmpty of
+                      true -> %% remove a partition
+                          remove_partition(Solution);
+                      false -> %% swap a partition with a new one
+                          Candidates = filter(Cur, Solution, Partitions),
+                          swap_partition(Solution, Candidates)
+                  end
+          end,
+    Score = case is_valid(Cur, New, Constraints) of
+                true ->
+                    score(Cur, New);
+                false ->
+                    0
+            end,
+    {New, Score}.
+
+%% Return a list of candidates selected from Partitions
+%% that are not in Cur or Solution
+filter(Cur, Solution, Partitions) ->
+    [Candidate || Candidate <- Partitions,
+                  not lists:member(Candidate, Cur),
+                  not lists:member(Candidate, Solution)].
+
+%% Add a random partition, from the list of candidates, to the solution
+add_partition(Solution, Candidates) ->
+    Partition = lists:nth(random:uniform(length(Candidates)), Candidates),
+    [Partition, Solution].
+
+remove_partition(Solution) ->
+    N = random:uniform(length(Solution)),
+    remove(N, Solution).
+
+remove(_, []) -> [];
+remove(1, [_|T]) -> T;
+remove(N, [H|T]) -> [H | remove(N-1, T)].
+
+swap_partition(Solution, Candidates) ->
+    N = random:uniform(length(Candidates)),
+    M = random:uniform(length(Solution)),
+    Partition = lists:nth(N),
+    replace(M, Partition, Solution).
+ 
+replace(_, _, []) -> [];
+replace(1, P, [_|T]) -> [P|T];
+replace(N, P, [H|T]) -> [H|replace(N-1, P, T)].
+
+is_valid(_CurWork, _NewWork, _Constraints) ->
+    false.
+
+%% Score is zero if it's not legal, otherwise it's the length of (Cur ++ New).
+score(Cur, New) ->
+    length(Cur) + length(New).
 
 
 %%%%%%%%%%%%%%%%%%%%%
 %% Eunit Tests
 %%%%%%%%%%%%%%%%%%%%%
 -ifdef(TEST).
+
+score_test() ->
+    ?assert(score([1,2,3,4], [1,2]) == 6),
+    ?assert(score([1,2,3,4], []) == 4).
+
+replace_test() ->
+    A = [1, 2, 3, 4],
+    B = replace(1, 9, A),
+    C = replace(2, 9, A),
+    D = replace(3, 9, A),
+    E = replace(4, 9, A),
+    ?assert(B == [9, 2, 3, 4]),
+    ?assert(C == [1, 9, 3, 4]),
+    ?assert(D == [1, 2, 9, 4]),
+    ?assert(E == [1, 2, 3, 9]).
+
+remove_test() ->
+    A = [1, 2, 3, 4],
+    B = remove(1, A),
+    C = remove(2, A),
+    D = remove(3, A),
+    E = remove(4, A),
+    ?assert(B == [2, 3, 4]),
+    ?assert(C == [1, 3, 4]),
+    ?assert(D == [1, 2, 4]),
+    ?assert(E == [1, 2, 3]).
+
+randomize_schedule_test() ->
+    Cur = [],
+    Solution = [],
+    Partitions = [],
+    Temp = 100.0,
+    Constraints = #constraints{max_sources=1, max_sinks=1, max_total=20,
+                               sources=[], sinks=[] },
+    {New, Score} = randomize_schedule(Cur, Solution, Partitions, Constraints, Temp),
+    ?assert(Score == 0),
+    ?assert(New == []).
+
+annealTemp_test() ->
+    Cur = [],
+    Solution = [],
+    Partitions = [],
+    Temp = 100.0,
+    Constraints = #constraints{max_sources=1, max_sinks=1, max_total=20,
+                               sources=[], sinks=[] },
+    Work = annealTemp(Cur, Solution, Partitions, Constraints, Temp, ?MAX_ITERATIONS),
+    ?assert(Work == []).
+
+anneal_test() ->
+    Constraints = #constraints{max_sources=1, max_sinks=1, max_total=20,
+                               sources=[], sinks=[] },
+    CurrentWork = [],
+    Partitions = [],
+    Work = anneal(CurrentWork, [], Partitions, Constraints, ?MAX_TEMP),
+    ?assert(Work == []).
 
 empty_schedule_test() ->
     CurrentWork = [],
@@ -86,7 +222,6 @@ schedule_1_test() ->
     TotalMax = 2000,
     Work = schedule(PriorWork, Partitions, SourceOwners, SinkOwners, SourceMax, SinkMax, TotalMax),
     ?assert(Work == []).
-
 
 partitions() ->
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].
