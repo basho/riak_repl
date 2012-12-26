82007cd8cebb022ae1af077b444067585b66c09f
Micah Warren <mwarren@basho.com>
2012-12-26 11:43:35 -0600

    Refactored service_mgr tests to use setup fixture

--- test/riak_core_service_mgr_tests.erl
+++ test/riak_core_service_mgr_tests.erl
@@ -26,62 +26,89 @@
                       {reuseaddr, true},
                       {active, false}]).
 
-%% this test runs first and leaves the server running for other tests
-start_link_test() ->
-    %% normally, ranch would be started as part of a supervisor tree, but we
-    %% need to start it here so that a supervision tree will be created.
-    ok = application:start(ranch),
-    {Ok, _Pid} = riak_core_service_mgr:start_link(?TEST_ADDR),
-    ?assert(Ok == ok).
+service_test_() ->
+    {timeout, 60000, {setup, fun() ->
+        ok = application:start(ranch),
+        {ok, _Pid} = riak_core_service_mgr:start_link(?TEST_ADDR)
+    end,
+    fun(_) ->
+        case whereis(riak_core_service_manager) of
+            Pid when is_pid(Pid) ->
+                riak_core_service_mgr:stop(),
+                {ok, Mon} = erlang:monitor(process, Pid),
+                receive
+                    {'DOWN', _, _, _, _} ->
+                        ok
+                after
+                    1000 ->
+                        ok
+                end;
+            undefined ->
+                ok
+        end
+    end,
+    fun(_) -> [
 
-get_services_test() ->
-    Services = gen_server:call(riak_core_service_manager, get_services),
-    ?assert([] == Services).
+        {"started", ?_assert(is_pid(whereis(riak_core_service_manager)))},
 
-%% register a service and confirm added
-register_service_test() ->
-    ExpectedRevs = [{1,0}, {1,0}],
-    TestProtocol = {{testproto, [{1,0}]}, {?TCP_OPTIONS, ?MODULE, testService, ExpectedRevs}},
-    riak_core_service_mgr:register_service(TestProtocol, {round_robin,?MAX_CONS}),
-    ?assert(riak_core_service_mgr:is_registered(testproto) == true).
+        {"get services", ?_assertEqual([], gen_server:call(riak_core_service_manager, get_services))},
 
-%% unregister and confirm removed
-unregister_service_test() ->
-    TestProtocolId = testproto,
-    riak_core_service_mgr:unregister_service(TestProtocolId),
-    ?assert(riak_core_service_mgr:is_registered(testproto) == false).
+        {"register service", fun() ->
+            ExpectedRevs = [{1,0}, {1,0}],
+            TestProtocol = {{testproto, [{1,0}]}, {?TCP_OPTIONS, ?MODULE, testService, ExpectedRevs}},
+            riak_core_service_mgr:register_service(TestProtocol, {round_robin,?MAX_CONS}),
+            ?assert(riak_core_service_mgr:is_registered(testproto))
+        end},
 
-register_stats_fun_test() ->
-    Fun = fun(Stats) ->
-                  ?assert(Stats == [{testproto,0}]) end,
-    riak_core_service_mgr:register_stats_fun(Fun).
+        {"unregister service", fun() ->
+            TestProtocolId = testproto,
+            riak_core_service_mgr:unregister_service(TestProtocolId),
+            ?assertNot(riak_core_service_mgr:is_registered(testproto))
+        end},
 
-%% start a service via normal sequence
-start_service_test() ->
-    %% re-register the test protocol and confirm registered
-    register_service_test(),
-    %% try to connect via a client that speaks our test protocol
-    ExpectedRevs = {expectedToPass, [{1,0}, {1,0}]},
-    riak_core_connection:connect(?TEST_ADDR, {{testproto, [{1,0}]},
-                                              {?TCP_OPTIONS, ?MODULE, ExpectedRevs}}),
-    %% allow client and server to connect and make assertions of success/failure
-    timer:sleep(1000),
-    Stats = riak_core_service_mgr:get_stats(),
-    ?assert(Stats == [{testproto,0}]).
+        {"register stats fun", fun() ->
+            Self = self(),
+            Fun = fun(Stats) ->
+                Self ! Stats
+            end,
+            riak_core_service_mgr:register_stats_fun(Fun),
+            GotStats = receive
+                Term ->
+                    Term
+            after 5500 ->
+                timeout
+            end,
+            ?assertEqual([], GotStats)
+        end},
 
-pause_existing_services_test() ->
-    riak_core_service_mgr:stop(),
-    %% there should be no services running now.
-    %% now start a client and confirm failure to connect
-    ExpectedArgs = expectedToFail,
-    riak_core_connection:connect(?TEST_ADDR, {{testproto, [{1,0}]},
-                                              {?TCP_OPTIONS, ?MODULE, ExpectedArgs}}),
-    %% allow client and server to connect and make assertions of success/failure
-    timer:sleep(1000).
+        {"start service test", fun() ->
+            %% re-register the test protocol and confirm registered
+            TestProtocol = {{testproto, [{1,0}]}, {?TCP_OPTIONS, ?MODULE, testService, [{1,0}, {1,0}]}},
+            riak_core_service_mgr:register_service(TestProtocol, {round_robin, ?MAX_CONS}),
+            ?assert(riak_core_service_mgr:is_registered(testproto)),
+            %register_service_test_d(),
+            %% try to connect via a client that speaks our test protocol
+            ExpectedRevs = {expectedToPass, [{1,0}, {1,0}]},
+            riak_core_connection:connect(?TEST_ADDR, {{testproto, [{1,0}]},
+                                                      {?TCP_OPTIONS, ?MODULE, ExpectedRevs}}),
+            %% allow client and server to connect and make assertions of success/failure
+            timer:sleep(1000),
+            Stats = riak_core_service_mgr:get_stats(),
+            ?assertEqual([{testproto,0}], Stats)
+        end},
 
-cleanup_test() ->
-    %riak_core_service_mgr:stop(),
-    application:stop(ranch).
+        {"pause existing services", fun() ->
+            riak_core_service_mgr:stop(),
+            %% there should be no services running now.
+            %% now start a client and confirm failure to connect
+            ExpectedArgs = expectedToFail,
+            riak_core_connection:connect(?TEST_ADDR, {{testproto, [{1,0}]},
+                                                      {?TCP_OPTIONS, ?MODULE, ExpectedArgs}}),
+            %% allow client and server to connect and make assertions of success/failure
+            timer:sleep(1000)
+        end}
+
+    ] end} }.
 
 %%------------------------
 %% Helper functions
