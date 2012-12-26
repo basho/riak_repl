f9ba0b70b6babf34bf290faebe631f71ecb020be
Micah Warren <mwarren@basho.com>
2012-12-26 12:06:39 -0600

    Refactored connection tests to use setup fixture.

--- test/riak_core_connection_tests.erl
+++ test/riak_core_connection_tests.erl
@@ -46,61 +46,65 @@ connect_failed({Proto,_Vers}, {error, Reason}, Args) ->
     ?assert(Reason == protocol_version_not_supported),
     ?assert(Proto == test1protoFailed).
 
-%% this test runs first and leaves the server running for other tests
-start_link_test() ->
-    %% normally, ranch would be started as part of a supervisor tree, but we
-    %% need to start it here so that a supervision tree will be created.
-    ok = application:start(ranch),
-    {Ok, _Pid} = riak_core_service_mgr:start_link(?TEST_ADDR),
-    ?assert(Ok == ok).
+conection_test_() ->
+    {timeout, 60000, {setup, fun() ->
+        ok = application:start(ranch),
+        {ok, _} = riak_core_service_mgr:start_link(?TEST_ADDR)
+    end,
+    fun(_) ->
+        riak_core_service_mgr:stop(),
+        application:stop(ranch)
+    end,
+    fun(_) -> [
 
-%% set/get the local cluster's name
-set_get_name_test() ->
-    riak_core_connection:set_symbolic_clustername("undefined"),
-    MyName = riak_core_connection:symbolic_clustername(),
-    ?assert("undefined" == MyName).
+        {"started", ?_assert(is_pid(whereis(riak_core_service_manager)))},
 
-%% register a service and confirm added
-register_service_test() ->
-    ExpectedRevs = [{1,0}, {1,1}],
-    ServiceProto = {test1proto, [{2,1}, {1,0}]},
-    ServiceSpec = {ServiceProto, {?TCP_OPTIONS, ?MODULE, test1service, ExpectedRevs}},
-    riak_core_service_mgr:register_service(ServiceSpec, {round_robin,?MAX_CONS}),
-    ?assert(riak_core_service_mgr:is_registered(test1proto) == true).
+        {"set and get name", fun() ->
+            riak_core_connection:set_symbolic_clustername("undefined"),
+            Got = riak_core_connection:symbolic_clustername(),
+            ?assertEqual("undefined", Got)
+        end},
 
-%% unregister and confirm removed
-unregister_service_test() ->
-    TestProtocolId = test1proto,
-    riak_core_service_mgr:unregister_service(TestProtocolId),
-    ?assert(riak_core_service_mgr:is_registered(TestProtocolId) == false).
+        {"register service", fun() ->
+            ExpectedRevs = [{1,0}, {1,1}],
+            ServiceProto = {test1proto, [{2,1}, {1,0}]},
+            ServiceSpec = {ServiceProto, {?TCP_OPTIONS, ?MODULE, test1service, ExpectedRevs}},
+            riak_core_service_mgr:register_service(ServiceSpec, {round_robin,?MAX_CONS}),
+            ?assert(riak_core_service_mgr:is_registered(test1proto))
+        end},
 
-protocol_match_test() ->
-    %% re-register the test protocol and confirm registered
-    register_service_test(),
-    %% try to connect via a client that speaks 0.1 and 1.1
-    ClientProtocol = {test1proto, [{0,1},{1,1}]},
-    ClientSpec = {ClientProtocol, {?TCP_OPTIONS, ?MODULE, [{1,1},{1,0}]}},
-    riak_core_connection:connect(?TEST_ADDR, ClientSpec),
+        {"unregister service", fun() ->
+            TestProtocolId = test1proto,
+            riak_core_service_mgr:unregister_service(TestProtocolId),
+            ?assertNot(riak_core_service_mgr:is_registered(TestProtocolId))
+        end},
 
-    timer:sleep(1000).
+        {"protocol match", fun() ->
+            ExpectedRevs = [{1,0}, {1,1}],
+            ServiceProto = {test1proto, [{2,1}, {1,0}]},
+            ServiceSpec = {ServiceProto, {?TCP_OPTIONS, ?MODULE, test1service, ExpectedRevs}},
+            riak_core_service_mgr:register_service(ServiceSpec, {round_robin,?MAX_CONS}),
 
-%% test that a mismatch of client and host args will notify both host and client
-%% of a failed negotiation.
-failed_protocol_match_test() ->
-    %% start service
-    SubProtocol = {{test1protoFailed, [{2,1}, {1,0}]},
-                   {?TCP_OPTIONS, ?MODULE, test1service, failed_host_args}},
-    riak_core_service_mgr:register_service(SubProtocol, {round_robin,?MAX_CONS}),
-    ?assert(riak_core_service_mgr:is_registered(test1protoFailed) == true),
+            % test protocal match
+            ClientProtocol = {test1proto, [{0,1},{1,1}]},
+            ClientSpec = {ClientProtocol, {?TCP_OPTIONS, ?MODULE, [{1,1},{1,0}]}},
+            riak_core_connection:connect(?TEST_ADDR, ClientSpec),
+            timer:sleep(1000)
+        end},
 
-    %% try to connect via a client that speaks 0.1 and 3.1. No Match with host!
-    ClientProtocol = {test1protoFailed, [{0,1},{3,1}]},
-    ClientSpec = {ClientProtocol, {?TCP_OPTIONS, ?MODULE, failed_client_args}},
-    riak_core_connection:connect(?TEST_ADDR, ClientSpec),
+        {"failed protocal match", fun() ->
+            %% start service
+            SubProtocol = {{test1protoFailed, [{2,1}, {1,0}]},
+                           {?TCP_OPTIONS, ?MODULE, test1service, failed_host_args}},
+            riak_core_service_mgr:register_service(SubProtocol, {round_robin,?MAX_CONS}),
+            ?assert(riak_core_service_mgr:is_registered(test1protoFailed) == true),
 
-    timer:sleep(2000),
-    ok.
+            %% try to connect via a client that speaks 0.1 and 3.1. No Match with host!
+            ClientProtocol = {test1protoFailed, [{0,1},{3,1}]},
+            ClientSpec = {ClientProtocol, {?TCP_OPTIONS, ?MODULE, failed_client_args}},
+            riak_core_connection:connect(?TEST_ADDR, ClientSpec),
 
-cleanup_test() ->
-    riak_core_service_mgr:stop(),
-    application:stop(ranch).
+            timer:sleep(2000)
+        end}
+
+    ] end } }.
