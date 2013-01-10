577b72998eaaeec9b7fd38fdfef8d2aad794520d
Micah Warren <mwarren@basho.com>
2013-01-10 16:09:04 -0600

    Simple conneciton put into foreach.

--- test/riak_repl2_rt_source_sink_tests.erl
+++ test/riak_repl2_rt_source_sink_tests.erl
@@ -10,47 +10,63 @@
 
 -define(SINK_PORT, 5007).
 -define(SOURCE_PORT, 4007).
--define(PROTO_V1_SOURCE_V1_SINK, {realtime, {1, 0}, {1, 0}}).
+-define(VER1, {1,0}).
+-define(PROTOCOL(NegotiatedVer), {realtime, NegotiatedVer, NegotiatedVer}).
+-define(PROTOCOL_V1, ?PROTOCOL(?VER1)).
+-define(PROTO_V1_SOURCE_V1_SINK, ?PROTOCOL(?VER1)).
 
 -record(v1_source_v1_sink, {
     sink, source, tcp_mon, rt
 }).
 
-v1_source_v1_sink_test_() ->
-    {setup, fun() ->
+-record(connection_tests, {
+    tcp_mon, rt
+}).
+
+connection_test_() ->
+    {foreach, fun() ->
         abstract_gen_tcp(),
         {ok, RT} = riak_repl2_rt:start_link(),
         {ok, _} = riak_repl2_rtq:start_link(),
         {ok, TCPMon} = riak_core_tcp_mon:start_link(),
-        {ok, ListenPid} = start_sink(),
-        {ok, {Source, Sink}} = start_source(),
-        #v1_source_v1_sink{sink = Sink, source = Source, tcp_mon = TCPMon, rt = RT}
+        #connection_tests{tcp_mon = TCPMon, rt = RT}
     end,
     fun(State) ->
-        #v1_source_v1_sink{sink = Sink, source = Source, tcp_mon = TCPMon,
-            rt = RT} = State,
+        #connection_tests{tcp_mon = TCPMon, rt = RT} = State,
         unlink(TCPMon),
         exit(TCPMon, kill),
         unlink(RT),
         exit(RT, kill),
-        meck:unload(riak_core_service_mgr),
-        meck:unload(riak_core_connection_mgr),
-        riak_repl2_rtsource_conn:stop(Source),
-        riak_repl2_rtsink_conn:stop(Sink),
+        %meck:unload(riak_core_service_mgr),
+        %meck:unload(riak_core_connection_mgr),
         riak_repl2_rtq:shutdown(),
         meck:unload(gen_tcp)
-    end,
-    fun(State) -> [
+    end, [
 
-        {"Everything started okay", fun() ->
-            #v1_source_v1_sink{sink = Sink, source = Source} = State,
-            ?assert(is_pid(Sink)),
-            ?assert(is_pid(Source)),
-            ?assert(is_process_alive(Sink)),
-            ?assert(is_process_alive(Source))
-        end}
+        fun(_) -> {"v1 to v1", fun() ->
+            {ok, _ListenPid} = start_sink(?VER1),
+            {ok, {Source, Sink}} = start_source(?VER1),
+            assert_living_pids([Source, Sink]),
+            connection_test_teardown_pids(Source, Sink)
+        end} end
 
-    ] end}.
+    ]}.
+
+assert_living_pids([]) ->
+    true;
+assert_living_pids([Pid | Tail]) ->
+    ?assert(is_pid(Pid)),
+    ?assert(is_process_alive(Pid)),
+    assert_living_pids(Tail).
+
+%% The start for both source and sink start up the mecks, which link to
+%% the calling process. This means the meck needs to be unloaded before that
+%% process exits, or there will be a big dump in the console for no reason.
+connection_test_teardown_pids(Source, Sink) ->
+    meck:unload(riak_core_service_mgr),
+    meck:unload(riak_core_connection_mgr),
+    riak_repl2_rtsource_conn:stop(Source),
+    riak_repl2_rtsink_conn:stop(Sink).
 
 abstract_gen_tcp() ->
     meck:new(gen_tcp, [unstick, passthrough]),
@@ -65,6 +81,9 @@ abstract_rt() ->
     end).
 
 start_sink() ->
+    start_sink(?VER1).
+
+start_sink(Version) ->
     TellMe = self(),
     meck:new(riak_core_service_mgr, [passthrough]),
     meck:expect(riak_core_service_mgr, register_service, fun(HostSpec, _Strategy) ->
@@ -72,7 +91,7 @@ start_sink() ->
         {ok, Listen} = gen_tcp:listen(?SINK_PORT, TcpOpts),
         TellMe ! sink_listening,
         {ok, Socket} = gen_tcp:accept(Listen),
-        {ok, Pid} = riak_repl2_rtsink_conn:start_link(?PROTO_V1_SOURCE_V1_SINK, "source_cluster"),
+        {ok, Pid} = riak_repl2_rtsink_conn:start_link(?PROTOCOL(Version), "source_cluster"),
         %unlink(Pid),
         ok = gen_tcp:controlling_process(Socket, Pid),
         ok = riak_repl2_rtsink_conn:set_socket(Pid, Socket, gen_tcp),
@@ -90,12 +109,15 @@ listen_sink() ->
     riak_repl2_rtsink_conn:register_service().
 
 start_source() ->
+    start_source(?VER1).
+
+start_source(NegotiatedVer) ->
     meck:new(riak_core_connection_mgr, [passthrough]),
     meck:expect(riak_core_connection_mgr, connect, fun(_ServiceAndRemote, ClientSpec) ->
         spawn_link(fun() ->
             {_Proto, {TcpOpts, Module, Pid}} = ClientSpec,
             {ok, Socket} = gen_tcp:connect("localhost", ?SINK_PORT, TcpOpts),
-            ok = Module:connected(Socket, gen_tcp, {"localhost", ?SINK_PORT}, ?PROTO_V1_SOURCE_V1_SINK, Pid, [])
+            ok = Module:connected(Socket, gen_tcp, {"localhost", ?SINK_PORT}, ?PROTOCOL(?VER1), Pid, [])
         end),
         {ok, make_ref()}
     end),
