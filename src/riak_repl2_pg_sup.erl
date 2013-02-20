b1f8d4d7b40d9f097094e7e775f77563afd31149
Dave Parfitt <dparfitt@basho.com>
2013-02-20 16:16:26 -0500

    found a typo in a supervisor

--- src/riak_repl2_pg_sup.erl
+++ src/riak_repl2_pg_sup.erl
@@ -20,7 +20,7 @@ init([]) ->
   [ {riak_repl2_pg_block_provider_sup,
     { riak_repl2_pg_block_provider_sup, start_link, []},
     permanent, infinity, supervisor, [
-    riak_repl_pg_block_provuder_sup]},
+    riak_repl_pg_block_provider_sup]},
 
   {riak_repl2_pg_block_requester_sup,
     {riak_repl2_pg_block_requester_sup, start_link, []},
