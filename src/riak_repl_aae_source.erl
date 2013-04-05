8a21b93d3caddb64f495ecd262c7d4b93e688338
Chris Tilt <chris.tilt@gmail.com>
2013-04-05 13:05:02 -0700

    refactored to fold over key diffs with helper function + comments

--- src/riak_repl_aae_source.erl
+++ src/riak_repl_aae_source.erl
@@ -113,8 +113,8 @@ prepare_exchange(start_exchange, State=#state{transport=Transport,
         ok ->
             %% TODO: Normal AAE has a timeout in this phase of the
             %%       protocol. Do we want similar for fullsync?
-            ok = send_msg(?MSG_INIT, {Partition, IndexN}, State),
-            case send_msg(?MSG_LOCK_TREE, State) of
+            ok = send_synchronous_msg(?MSG_INIT, {Partition, IndexN}, State),
+            case send_synchronous_msg(?MSG_LOCK_TREE, State) of
                 ok ->
                     update_trees(start_exchange, State);
                 Error ->
@@ -134,11 +134,8 @@ prepare_exchange(start_exchange, State=#state{transport=Transport,
 update_trees(start_exchange, State=#state{tree_pid=TreePid,
                                           index=Partition,
                                           index_n=IndexN}) ->
-    %% lager:debug("Sending to ~p", [LocalVN]),
-    %% lager:debug("Sending to ~p", [RemoteVN]),
-
     update_request(TreePid, {Partition, undefined}, IndexN),
-    case send_msg(?MSG_UPDATE_TREE, State) of
+    case send_synchronous_msg(?MSG_UPDATE_TREE, State) of
         ok ->
             update_trees({tree_built, Partition, IndexN}, State);
         not_responsible ->
@@ -146,38 +143,49 @@ update_trees(start_exchange, State=#state{tree_pid=TreePid,
     end;
 
 update_trees({not_responsible, VNodeIdx, IndexN}, State) ->
-    lager:debug("VNode ~p does not cover preflist ~p", [VNodeIdx, IndexN]),
+    lager:info("VNode ~p does not cover preflist ~p", [VNodeIdx, IndexN]),
     send_exchange_status({not_responsible, VNodeIdx, IndexN}, State),
     {stop, not_responsible, State};
 update_trees({tree_built, _, _}, State) ->
     Built = State#state.built + 1,
     case Built of
         2 ->
-            lager:debug("Moving to key exchange"),
+            lager:info("Moving to key exchange state"),
             {next_state, key_exchange, State, 0};
         _ ->
             {next_state, update_trees, State#state{built=Built}}
     end.
 
 %% @doc Now that locks have been acquired and both hashtrees have been updated,
-%%      perform a key exchange and trigger read repair for any divergent keys.
+%%      perform a key exchange and trigger replication for any divergent keys.
 key_exchange(timeout, State=#state{cluster=Cluster,
                                    transport=Transport,
                                    socket=Socket,
                                    index=Partition,
                                    tree_pid=TreePid,
                                    index_n=IndexN}) ->
-    lager:debug("Starting fullsync exchange with ~p for ~p/~p",
+    lager:info("Starting fullsync exchange with ~p for ~p/~p",
                 [Cluster, Partition, IndexN]),
 
     SourcePid = self(),
+
+    %% A function that receives callbacks from the hashtree:compare engine.
+    %% This will send messages to ourself, handled in compare_loop(), that
+    %% allow us to pass control of the TCP socket around. This is needed so
+    %% that the process needing to send/receive on that socket has ownership
+    %% of it.
     Remote = fun(init, _) ->
+                     %% cause control of the socket to be given to AAE so that
+                     %% the get_bucket and key_hashes can send messages via the
+                     %% socket (with correct ownership).
                      SourcePid ! {'$aae_src', worker_pid, self()};
                 (get_bucket, {L, B}) ->
-                     send_msg(?MSG_GET_AAE_BUCKET, {L,B}, State);
+                     %% 
+                     send_synchronous_msg(?MSG_GET_AAE_BUCKET, {L,B}, State);
                 (key_hashes, Segment) ->
-                     send_msg(?MSG_GET_AAE_SEGMENT, Segment, State);
+                     send_synchronous_msg(?MSG_GET_AAE_SEGMENT, Segment, State);
                 (final, _) ->
+                     %% give ourself control of the socket again
                      ok = Transport:controlling_process(Socket, SourcePid)
              end,
 
@@ -186,17 +194,19 @@ key_exchange(timeout, State=#state{cluster=Cluster,
     %% key differences, exchange can take arbitrarily long. For now, go with
     %% unbounded exchange, with the ability to cancel exchanges through the
     %% entropy manager if needed.
-    AccFun = fun(KeyDiff, Acc) ->
-                     lager:info("Keydiff: ~p", [KeyDiff]),
-                     case Acc of
-                         [] ->
-                             [1];
-                         [Count] ->
-                             [Count+1];
-                         Other ->
-                             Other
-                     end
+
+    %% accumulates a list of one element that is the count of
+    %% keys that differed. We can't prime the accumulator. It
+    %% always starts as the empty list. KeyDiffs is a list of hashtree::keydiff()
+    AccFun = fun(KeyDiffs, Acc0) ->
+                     %% fold over key differences. We could batch up all the diffs
+                     %% and send as a giant blob, but let's do one at a time for now.
+                     lists:foldl(fun(KeyDiff, AccIn) ->
+                                         replicate_diff(KeyDiff, AccIn, State) end,
+                                 Acc0,
+                                 KeyDiffs)
              end,
+
     %% TODO: Add stats for AAE
     spawn_link(fun() ->
                        Acc = riak_kv_index_hashtree:compare(IndexN, Remote, AccFun, TreePid),
@@ -210,10 +220,10 @@ key_exchange(timeout, State=#state{cluster=Cluster,
             %% exchange_complete(LocalVN, RemoteVN, IndexN, 0),
             lager:info("Repl'd 0 keys"),
             ok;
-        [_Count] ->
+        [Count] ->
             %% exchange_complete(LocalVN, RemoteVN, IndexN, Count),
             lager:info("Repl'd ~b keys during fullsync to ~p of ~p/~p ",
-                       [Cluster, Partition, IndexN])
+                       [Count, Cluster, Partition, IndexN])
     end,
     {stop, normal, State2}.
 
@@ -230,6 +240,33 @@ compare_loop(State=#state{transport=Transport,
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+%% @private
+%% Returns accumulator as a list of one element that is the count of
+%% keys that differed. Initial value of Acc is always [].
+replicate_diff(KeyDiff, Acc, #state{cluster=_Cluster,
+                                    transport=_Transport,
+                                    socket=_Socket,
+                                    index=Partition,
+                                    tree_pid=_TreePid,
+                                    index_n=_IndexN}) ->
+    case KeyDiff of
+        {remote_missing, BObj} ->
+            {Bucket,Key} = binary_to_term(BObj),
+            lager:info("Keydiff: remote partition ~p missing: ~p:~p", [Partition, Bucket, Key]);
+        Other ->
+            lager:info("Keydiff: ~p", [Other])
+    end,
+
+    case Acc of
+        [] ->
+            [1];
+        [Count] ->
+            %% accrue number of differences in this segment
+            [Count+1];
+        _Other ->
+            Acc
+    end.
 
 %% @private
 update_request(Tree, {Index, _}, IndexN) ->
@@ -276,14 +313,20 @@ send_exchange_status(Status, State) ->
 %%     rpc:call(RemoteNode, riak_kv_entropy_info, exchange_complete,
 %%              [RemoteIdx, LocalIdx, IndexN, Repaired]).
 
-send_msg(MsgType, Msg, State=#state{transport=Transport,
-                                    socket=Socket}) ->
+
+%%------------
+%% Synchronous messaging with the AAE fullsync "sink" on the remote cluster
+%%------------
+%% send a tagged message with type and data and return the reply
+send_synchronous_msg(MsgType, Msg, State=#state{transport=Transport,
+                                                socket=Socket}) ->
     Data = term_to_binary(Msg),
     ok = Transport:send(Socket, <<MsgType:8, Data/binary>>),
     get_reply(State).
 
-send_msg(MsgType, State=#state{transport=Transport,
-                               socket=Socket}) ->
+%% send a message with type tag only, no data
+send_synchronous_msg(MsgType, State=#state{transport=Transport,
+                                           socket=Socket}) ->
     ok = Transport:send(Socket, <<MsgType:8>>),
     get_reply(State).
 
