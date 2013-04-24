ef2bc78c09a6faf25d285916a85ca2330c86ba68
Chris Tilt <chris.tilt@gmail.com>
2013-04-24 14:59:58 -0700

    Add default registration functions to cluster manager supervisor

--- src/riak_core_cluster_mgr_sup.erl
+++ src/riak_core_cluster_mgr_sup.erl
@@ -18,6 +18,9 @@ start_link() ->
 %% @spec init([]) -> SupervisorTree
 %% @doc supervisor callback.
 init([]) ->
+    ClusterMgrDefaults = [fun riak_repl_app:cluster_mgr_member_fun/1,
+                          fun riak_repl_app:cluster_mgr_write_cluster_members_to_ring/2,
+                          fun riak_repl_app:cluster_mgr_read_cluster_targets_from_ring/0],
     Processes =
         [%% TODO: move these to riak core
          %% Service Manager (inbound connections)
@@ -33,7 +36,7 @@ init([]) ->
          {riak_core_cluster_conn_sup, {riak_core_cluster_conn_sup, start_link, []},
           permanent, infinity, supervisor, [riak_core_cluster_conn_sup]},
          %% Cluster Manager
-         {riak_repl_cluster_mgr, {riak_core_cluster_mgr, start_link, []},
+         {riak_repl_cluster_mgr, {riak_core_cluster_mgr, start_link, ClusterMgrDefaults},
           permanent, 5000, worker, [riak_core_cluster_mgr]},
          {riak_core_tcp_mon, {riak_core_tcp_mon, start_link, []},
           permanent, 5000, worker, [riak_core_tcp_mon]}
