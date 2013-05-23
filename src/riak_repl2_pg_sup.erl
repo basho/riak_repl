a444ec72a01470b0cf91194365f2bb3d1e047b20
Dave Parfitt <dparfitt@basho.com>
2013-05-23 17:50:04 -0400

    changed block_provider to temporary from perm

--- src/riak_repl2_pg_sup.erl
+++ src/riak_repl2_pg_sup.erl
@@ -24,7 +24,7 @@ init([]) ->
 
   {riak_repl2_pg_block_requester_sup,
     {riak_repl2_pg_block_requester_sup, start_link, []},
-    permanent, infinity, supervisor,
+    temporary, infinity, supervisor,
     [riak_repl2_pg_block_requester_sup]},
 
   {riak_repl2_pg, {riak_repl2_pg, start_link, []},
