613ac67349f56cf3cfbaa3c7c800c9616904fb17
Andrew Thompson <andrew@hijacked.us>
2013-05-28 17:56:21 -0400

    Fix RTQ test

--- test/riak_repl2_rtq_test.erl
+++ test/riak_repl2_rtq_test.erl
@@ -12,10 +12,10 @@ rtq_trim_test() ->
         MyBin = crypto:rand_bytes(1024*1024),
         [gen_server:cast(Pid, {push, 1, MyBin}) || _ <- lists:seq(0, 20)],
 
-        %% we should get 9 elements back, when we ask for them, because of
-        %% overhead
-        Size = accumulate(Pid, 0, 9),
-        ?assert(Size < 100*1024*1024),
+        %% we get all 10 bytes back, because in TEST mode the RTQ disregards
+        %% ETS overhead
+        Size = accumulate(Pid, 0, 10),
+        ?assert(Size =< 10*1024*1024),
         %% the queue is now empty
         ?assert(gen_server:call(Pid, {is_empty, rtq_test}))
     after
@@ -39,9 +39,8 @@ accumulate(Pid, Acc, C) ->
     ask(Pid),
     receive
         {rtq_entry, {N, B}} ->
-            Size1 = erts_debug:flat_size({N, B}),
-            Size2 = byte_size(B) - erts_debug:flat_size(B),
-            accumulate(Pid, Acc+Size1+Size2, C-1)
+            Size = byte_size(B),
+            accumulate(Pid, Acc+Size, C-1)
     end.
 
 
