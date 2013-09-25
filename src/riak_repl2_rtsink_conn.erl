a834c4de31b10193702e331b3e0f415e71b847c8
Micah Warren <mwarren@basho.com>
2013-09-25 10:21:36 -0500

    Fixed when ackedto == completed head.
    There is a possibiliy that the ackto value will equal completed due
    to skips. Without the fix, this means the ackto would never evaluate
    to a point where it can consume it. Now it can ack.

--- src/riak_repl2_rtsink_conn.erl
+++ src/riak_repl2_rtsink_conn.erl
@@ -328,6 +328,8 @@ reset_ref_seq(Seq, State) ->
 %% call.
 ack_to(Acked, []) ->
     {Acked, []};
+ack_to(Acked, [Acked | Completed]) ->
+    ack_to(Acked, Completed);
 ack_to(Acked, [Seq | Completed2] = Completed) ->
     case Acked + 1 of
         Seq ->
