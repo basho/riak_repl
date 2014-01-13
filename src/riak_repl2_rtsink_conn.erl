15114ca777c4a2d2d69ad2511844f57fbb4192a6
Jon Anderson <janderson@basho.com>
2014-01-13 12:34:00 -0500

    re-added unmeck call, and removed commented set_env

--- src/riak_repl2_rtsink_conn.erl
+++ src/riak_repl2_rtsink_conn.erl
@@ -472,8 +472,10 @@ cleanup(_Ctx) ->
     riak_repl_test_util:kill_and_wait(riak_repl2_rtq),
     riak_repl_test_util:kill_and_wait(riak_repl2_rt),
     riak_repl_test_util:stop_test_ring(),
-%    application:set_env(riak_repl, reactivate_socket_interval_millis,
-%          ?REACTIVATE_SOCK_INT_MILLIS),
+    riak_repl_test_util:maybe_unload_mecks(
+      [riak_core_service_mgr,
+       riak_core_connection_mgr,
+       gen_tcp]),
     meck:unload(),
     ok.
 
