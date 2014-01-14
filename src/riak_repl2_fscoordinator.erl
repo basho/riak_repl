23d99d0087626d486fd1825997ee773219d16fb4
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-01-14 15:48:33 -0500

    Remove bad specification.

--- src/riak_repl2_fscoordinator.erl
+++ src/riak_repl2_fscoordinator.erl
@@ -101,13 +101,13 @@ stop_fullsync(Pid) ->
 
 %% @doc Get a status report as a proplist for each fullsync enabled. Usually
 %% for use with a console.
--spec status() -> [tuple()].
 status() ->
     LeaderNode = riak_repl2_leader:leader_node(),
     case LeaderNode of
         undefined ->
             [];
-        _ -> [{Remote, status(Pid)} || {Remote, Pid} <-
+        _ ->
+            [{Remote, status(Pid)} || {Remote, Pid} <-
                 riak_repl2_fscoordinator_sup:started(LeaderNode)]
     end.
 
@@ -487,7 +487,7 @@ handle_socket_msg({location, Partition, {Node, Ip, Port}}, #state{whereis_waitin
         undefined ->
             State;
         {N, _OldNode, Tref} ->
-            erlang:cancel_timer(Tref),
+            _ = erlang:cancel_timer(Tref),
             Waiting2 = proplists:delete(Partition, Waiting),
             % we don't know for sure it's no longer busy until we get a busy reply
             NewBusies = sets:del_element(Node, State#state.busy_nodes),
@@ -504,7 +504,7 @@ handle_socket_msg({location_busy, Partition}, #state{whereis_waiting = Waiting} 
         {N, OldNode, Tref} ->
             lager:info("anya Partition ~p is too busy on cluster ~p at node ~p",
                        [Partition, State#state.other_cluster, OldNode]),
-            erlang:cancel_timer(Tref),
+            _ = erlang:cancel_timer(Tref),
             Waiting2 = proplists:delete(Partition, Waiting),
             State2 = State#state{whereis_waiting = Waiting2},
             Partition2 = {Partition, N, OldNode},
@@ -520,7 +520,7 @@ handle_socket_msg({location_busy, Partition, Node}, #state{whereis_waiting = Wai
             State;
         {N, _OldNode, Tref} ->
             lager:info("Partition ~p is too busy on cluster ~p at node ~p", [Partition, State#state.other_cluster, Node]),
-            erlang:cancel_timer(Tref),
+            _ = erlang:cancel_timer(Tref),
 
             Waiting2 = proplists:delete(Partition, Waiting),
             State2 = State#state{whereis_waiting = Waiting2},
@@ -540,7 +540,7 @@ handle_socket_msg({location_down, Partition}, #state{whereis_waiting=Waiting} = 
         {_N, _OldNode, Tref} ->
             lager:info("Partition ~p is unavailable on cluster ~p",
                 [Partition, State#state.other_cluster]),
-            erlang:cancel_timer(Tref),
+            _ = erlang:cancel_timer(Tref),
             Waiting2 = proplists:delete(Partition, Waiting),
             State2 = State#state{whereis_waiting = Waiting2},
             start_up_reqs(State2)
@@ -552,7 +552,7 @@ handle_socket_msg({location_down, Partition, _Node}, #state{whereis_waiting=Wait
         {_N, _OldNode, Tref} ->
             lager:info("Partition ~p is unavailable on cluster ~p",
                 [Partition, State#state.other_cluster]),
-            erlang:cancel_timer(Tref),
+            _ = erlang:cancel_timer(Tref),
             Waiting2 = proplists:delete(Partition, Waiting),
             State2 = State#state{whereis_waiting = Waiting2},
             start_up_reqs(State2)
