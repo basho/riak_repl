769661e5b4e235852d4acec0ff5ffc5684209589
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-01-14 16:12:20 -0500

    Explicit matches, formatting updates.

--- src/riak_repl2_pg_proxy_sup.erl
+++ src/riak_repl2_pg_proxy_sup.erl
@@ -1,12 +1,17 @@
-%%%-------------------------------------------------------------------
-%%% Created : 21 Feb 2013 by Dave Parfitt
-%%%-------------------------------------------------------------------
+%% Riak EnterpriseDS
+%% Copyright 2007-2014 Basho Technologies, Inc. All Rights Reserved.
+
 -module(riak_repl2_pg_proxy_sup).
 
 -behaviour(supervisor).
 
 %% API
--export([start_link/0, set_leader/2, started/1, start_proxy/1, make_remote/1, stop_proxy/2]).
+-export([start_link/0,
+         set_leader/2,
+         started/1,
+         start_proxy/1,
+         make_remote/1,
+         stop_proxy/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -17,6 +22,7 @@
 %%%===================================================================
 %%% API functions
 %%%===================================================================
+
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
@@ -25,8 +31,8 @@ set_leader(Node, _Pid) ->
         Node -> ok;
         _ ->
           [  begin
-                supervisor:terminate_child(?MODULE, Remote),
-                supervisor:delete_child(?MODULE, Remote)
+                ok = supervisor:terminate_child(?MODULE, Remote),
+                ok = supervisor:delete_child(?MODULE, Remote)
              end
                 || {Remote, Pid, _, _} <-
                       supervisor:which_children(?MODULE), is_pid(Pid)
@@ -39,7 +45,7 @@ start_proxy(Remote) ->
     supervisor:start_child({?MODULE, node()}, Childspec).
 
 stop_proxy(Node, Remote) ->
-    lager:debug("Stopping pg_proxy for ~p", [Remote]),    
+    lager:debug("Stopping pg_proxy for ~p", [Remote]),
     supervisor:terminate_child({?MODULE, Node}, Remote),
     supervisor:delete_child({?MODULE, Node}, Remote).
 
@@ -52,7 +58,5 @@ init(_) ->
 
 make_remote(Remote) ->
     Name = riak_repl_util:make_pg_proxy_name(Remote),
-    lager:debug("make_remote ~p", [Name]),
     {Name, {riak_repl2_pg_proxy, start_link, [Name]},
         transient, ?SHUTDOWN, worker, [riak_repl2_pg_proxy, pg_proxy]}.
-
