d396523a27c4ddf7a894fc6bcad1aaa56de9024a
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-02-06 05:39:34 -0500

    Match false.
    Don't match the error response, match false, as 
    the function never returns error.

--- src/riak_core_service_mgr.erl
+++ src/riak_core_service_mgr.erl
@@ -253,7 +253,7 @@ handle_info({'DOWN', Ref, process, Pid, _Reason}, State) ->
                 {value, {Ref, ProtocolId}, Rest} ->
                     gen_server:cast(?SERVER, {service_down_event, Pid, ProtocolId}),
                     Rest;
-                error ->
+                false ->
                     Refs
             end,
     {noreply, State#state{refs=Refs2}};
