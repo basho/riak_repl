5f317ea69ef6e3c55e5cf1a23e26793b51a83104
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-03-03 16:29:19 -0800

    Ignore return value of comprehension.
    Resolves:
    
    riak_core_cluster_mgr.erl:380: Expression produces a value of type [{pid(),'poll_cluster'}], but this value is unmatched

--- src/riak_core_cluster_mgr.erl
+++ src/riak_core_cluster_mgr.erl
@@ -377,7 +377,7 @@ handle_cast(_Unhandled, _State) ->
 %% it is time to poll all clusters and get updated member lists
 handle_info(poll_clusters_timer, State) when State#state.is_leader == true ->
     Connections = riak_core_cluster_conn_sup:connections(),
-    [Pid ! {self(), poll_cluster} || {_Remote, Pid} <- Connections],
+    _ = [Pid ! {self(), poll_cluster} || {_Remote, Pid} <- Connections],
     erlang:send_after(?CLUSTER_POLLING_INTERVAL, self(), poll_clusters_timer),
     {noreply, State};
 handle_info(poll_clusters_timer, State) ->
