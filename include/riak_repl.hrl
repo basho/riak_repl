1ba92bc58736d3ef4a590307eb23fc81b2e5b3ed
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-05-28 13:59:53 +0200

    Add new setting for number of reservation retries.

--- include/riak_repl.hrl
+++ include/riak_repl.hrl
@@ -26,6 +26,8 @@
 -define(DEFAULT_MAX_SINKS_NODE, 1).
 %% How many times during a fullsync we should try a partition
 -define(DEFAULT_SOURCE_RETRIES, infinity).
+%% How many times we should retry when failing a reservation
+-define(DEFAULT_RESERVE_RETRIES, 0).
 %% 20 seconds. sources should claim within 5 seconds, but give them a little more time
 -define(RESERVATION_TIMEOUT, (20 * 1000)).
 -define(DEFAULT_MAX_FS_BUSIES_TOLERATED, 10).
