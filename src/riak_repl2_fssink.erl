4d4d4ad8a9248c3fc591929ac38997b6d2400d63
Ian Milligan <ianmllgn@gmail.com>
2015-07-31 14:24:45 -0700

    Replace 'decode_obj_msg' with 'decode_bin_obj'.

--- src/riak_repl2_fssink.erl
+++ src/riak_repl2_fssink.erl
@@ -148,8 +148,9 @@ handle_info({Proto, Socket, Data},
         State=#state{socket=Socket,transport=Transport}) when Proto==tcp; Proto==ssl ->
     %% aae strategy will not receive messages here
     Transport:setopts(Socket, [{active, once}]),
-    case riak_repl_util:decode_obj_msg(Data) of
-        {fs_diff_obj, RObj} ->
+    case binary_to_term(Data) of
+        {fs_diff_obj, BinObj} ->
+            RObj = riak_repl_util:decode_bin_obj(BinObj),
             riak_repl_util:do_repl_put(RObj);
         Other ->
             gen_fsm:send_event(State#state.fullsync_worker, Other)
