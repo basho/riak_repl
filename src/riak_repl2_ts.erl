c135fd0a091d0abf7f5b974a809c10741a1a9b57
John R. Daily <git@epep.us>
2016-04-04 14:48:18 -0400

    More commentary

--- src/riak_repl2_ts.erl
+++ src/riak_repl2_ts.erl
@@ -6,7 +6,9 @@
 
 -include("riak_repl.hrl").
 
-%% Realtime replication hook for Timeseries
+%% Realtime replication hook for Timeseries. The basic ideas are
+%% copied from `riak_repl2_rt'
+
 postcommit({PK, Val}, Bucket, BucketProps) when not is_list(Val) ->
     postcommit({PK, [Val]}, Bucket, BucketProps);
 postcommit(PartitionBatch, Bucket, BucketProps) ->
@@ -18,10 +20,16 @@ maybe_postcommit(_PartitionBatch, _Bucket, NoRT) when NoRT == false orelse
 maybe_postcommit({PK, Vals}=PartitionBatch, Bucket, _RT) ->
     lager:debug("Timeseries batch sent to repl~n    PartIdx~p => ~p...", [PK, hd(Vals)]),
     Meta = set_bucket_meta(Bucket),
+
+    %% `w3' is the earliest wire protocol version to handle timeseries
+    %% data properly
     BinObj = riak_repl_util:to_wire(w3, PartitionBatch),
 
-    %% try the proxy first, avoids race conditions with unregister()
-    %% during shutdown
+    %% When a node starts shutting down, a process
+    %% `riak_repl2_rtq_proxy' is registered to forward realtime
+    %% updates to another node for delivery to the remote cluster. Try
+    %% that first to avoid race conditions with unregister() during
+    %% shutdown
     case whereis(riak_repl2_rtq_proxy) of
         undefined ->
             riak_repl2_rtq:push(length(Vals), BinObj, Meta);
@@ -30,6 +38,10 @@ maybe_postcommit({PK, Vals}=PartitionBatch, Bucket, _RT) ->
             riak_repl2_rtq_proxy:push(length(Vals), BinObj, Meta)
     end.
 
+%% Bucket properties will be checked at the remote cluster for
+%% compatibility. Legacy ("default") buckets would be a single
+%% argument to this instead of a tuple for bucket types, but we don't
+%% support timeseries data in legacy buckets.
 set_bucket_meta({Type, _}) ->
     M = orddict:new(),
     PropsHash = riak_repl_bucket_type_util:property_hash(Type),
